9967cb0f396d7c3bbf102b5549c20962bbd6634a
Douglas Gusson <douglasgusson@gmail.com>
2020-06-30 18:27:51 -0300

    Inclusão da migração de criação da tabela de filmes

---
+++ database/migrations/2020_06_30_175024_create_filmes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('filmes', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->integer('duracao');
+            $table->foreignId('genero_id');
+            $table->string('direcao');
+            $table->string('classificacao');
+            $table->longText('sinopse');
+            $table->date('lancamento');
+            $table->string('poster');
+            $table->foreign('genero_id')->references('id')->on('generos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('filmes');
+    }
+}
