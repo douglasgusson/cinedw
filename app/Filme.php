14523d034da7db07654f4615d6c951fa51c2f908
Douglas Gusson <douglasgusson@gmail.com>
2020-06-30 18:29:18 -0300

    Criação da classe model de Filme

---
+++ app/Filme.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Filme extends Model
+{
+
+    public function genero()
+    {
+        return $this->belongsTo(Genero::class, 'genero_id');
+    }
+}
