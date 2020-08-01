ad39fc738651e01f9f2cb01787b79793b9b1dfd3
Douglas Gusson <douglasgusson@gmail.com>
2020-08-01 08:07:18 -0300

    Criação listagem de filmes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,110 +13,70 @@
     <div class="container">
         <div class="row">
             <div class="col-md-9">
-                <h3 class="display-4">Filmes</h3>
+                <h3 class="display-4 mb-4">Filmes</h3>
 
                 <div class="row row-cols-1 row-cols-md-3">
 
+                    @foreach($filmes as $filme)
+
                     <div class="col mb-4">
                         <div class="card h-100">
-                            <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHnxj473p5FpApEGIkO5pSca_MlJFLx5_qM2e7JHb2R4VzGMHg" alt="Poster Annabelle 3: De Volta Para Casa">
+                            <img class="card-img-top" src="/uploads/{{ $filme->poster }}" alt="Poster {{ $filme->titulo }}">
                             <div class="card-body">
-                                <p class="card-text">Annabelle 3: De Volta Para Casa</p>
+                                <p class="card-text">{{ $filme->titulo }}</p>
                                 <div class="d-flex justify-content-between align-items-center mb-2">
-                                    <span class="badge badge-pill badge-dark">Terror</span>
-                                    <small class="text-muted">110min</small>
+                                    <span class="badge badge-pill badge-dark">{{ $filme->genero->nome }}</span>
+                                    <small class="text-muted">{{ $filme->duracao }} min</small>
                                 </div>
                                 <div class="text-muted mb-2">
-                                    Estreia: 27/06/2019
+                                    Estreia: {{ \Carbon\Carbon::parse($filme->lancamento)->format('d/m/Y') }}
+
                                 </div>
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver mais</button>
+                                    <a href="/filme/{{ $filme->id }}" class="btn btn-sm btn-outline-secondary">Ver mais</a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col mb-4">
-                        <div class="card h-100">
-                            <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHnxj473p5FpApEGIkO5pSca_MlJFLx5_qM2e7JHb2R4VzGMHg" alt="Poster Annabelle 3: De Volta Para Casa">
-                            <div class="card-body">
-                                <p class="card-text">Annabelle 3: De Volta Para Casa</p>
-                                <div class="d-flex justify-content-between align-items-center mb-2">
-                                    <span class="badge badge-pill badge-dark">Terror</span>
-                                    <small class="text-muted">110min</small>
-                                </div>
-                                <div class="text-muted mb-2">
-                                    Estreia: 27/06/2019
-                                </div>
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver mais</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col mb-4">
-                        <div class="card h-100">
-                            <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHnxj473p5FpApEGIkO5pSca_MlJFLx5_qM2e7JHb2R4VzGMHg" alt="Poster Annabelle 3: De Volta Para Casa">
-                            <div class="card-body">
-                                <p class="card-text">Annabelle 3: De Volta Para Casa</p>
-                                <div class="d-flex justify-content-between align-items-center mb-2">
-                                    <span class="badge badge-pill badge-dark">Terror</span>
-                                    <small class="text-muted">110min</small>
-                                </div>
-                                <div class="text-muted mb-2">
-                                    Estreia: 27/06/2019
-                                </div>
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver mais</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col mb-4">
-                        <div class="card h-100">
-                            <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHnxj473p5FpApEGIkO5pSca_MlJFLx5_qM2e7JHb2R4VzGMHg" alt="Poster Annabelle 3: De Volta Para Casa">
-                            <div class="card-body">
-                                <p class="card-text">Annabelle 3: De Volta Para Casa</p>
-                                <div class="d-flex justify-content-between align-items-center mb-2">
-                                    <span class="badge badge-pill badge-dark">Terror</span>
-                                    <small class="text-muted">110min</small>
-                                </div>
-                                <div class="text-muted mb-2">
-                                    Estreia: 27/06/2019
-                                </div>
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver mais</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col mb-4">
-                        <div class="card h-100">
-                            <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHnxj473p5FpApEGIkO5pSca_MlJFLx5_qM2e7JHb2R4VzGMHg" alt="Poster Annabelle 3: De Volta Para Casa">
-                            <div class="card-body">
-                                <p class="card-text">Annabelle 3: De Volta Para Casa</p>
-                                <div class="d-flex justify-content-between align-items-center mb-2">
-                                    <span class="badge badge-pill badge-dark">Terror</span>
-                                    <small class="text-muted">110min</small>
-                                </div>
-                                <div class="text-muted mb-2">
-                                    Estreia: 27/06/2019
-                                </div>
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver mais</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
+                    @endforeach
                 </div>
             </div>
 
             <div class="col-md-3">
                 <h3 class="display-5">Exibições</h3>
+                <div class="card d-block mb-4">
+                    <div class="card-header">
+                        Mulher-Maravilha
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item d-flex justify-content-around">
+                            <span>18:40</span>
+                            <span>Dublado</span>
+                            <span>3D</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card d-block mb-4">
+                    <div class="card-header">
+                        Tomb Raider: A Origem
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item d-flex justify-content-around">
+                            <span>19:25</span>
+                            <span>Legendado</span>
+                            <span>2D</span>
+                        </li>
+                    </ul>
+                </div>
+                <h3 class="display-5">Gêneros</h3>
+                <ul class="nav flex-column">
+                    @foreach($generos as $genero)
+                    <li class="nav-item">
+                        <a class="nav-link" href="/genero/{{ $genero->id }}">{{ $genero->nome }}</a>
+                    </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
